73ce1f07657f80997b76725b5ed5e74479d4d935
Erwan Régy <erwanregy02@gmail.com>
2023-02-21 12:13:32 +0000

    Added fixed struct to neural_network.sv

--- sources_1/new/neural_network.sv
+++ sources_1/new/neural_network.sv
@@ -6,7 +6,8 @@ typedef struct {
 } LAYER;
 
 module neural_network #(parameter
-    DATA_WIDTH = 32,
+    INTG_WIDTH = 16,
+    FRAC_WIDTH = 16,
     NUM_LAYERS = 3,
     NUM_INPUTS = 10,
     LAYER LAYERS[NUM_LAYERS] = '{
@@ -16,54 +17,62 @@ module neural_network #(parameter
     }
 ) (
     input logic clock, reset, inputs_ready,
-    input logic signed [DATA_WIDTH-1:0] inputs[NUM_INPUTS],
-    output logic signed [DATA_WIDTH-1:0] outputs[LAYERS[NUM_LAYERS-1].NUM_NEURONS],
+    input fixed inputs[NUM_INPUTS],
+    output fixed outputs[LAYERS[NUM_LAYERS-1].NUM_NEURONS],
     output logic outputs_ready
 );
+
+typedef struct packed {
+    logic signed [INTG_WIDTH-1:0] intg;
+    logic [FRAC_WIDTH-1:0] frac;
+} fixed;
 
 logic signed [DATA_WIDTH-1:0] layer_0_outputs[LAYERS[0].NUM_NEURONS];
 logic layer_0_outputs_ready;
 
 dense_layer #(
-   .DATA_WIDTH(DATA_WIDTH),
-   .NUM_INPUTS(NUM_INPUTS),
-   .NUM_NEURONS(LAYERS[0].NUM_NEURONS),
-   .ACTIVATION(LAYERS[0].ACTIVATION)
+    .INTG_WIDTH(INTG_WIDTH),
+    .FRAC_WIDTH(FRAC_WIDTH),
+    .NUM_INPUTS(NUM_INPUTS),
+    .NUM_NEURONS(LAYERS[0].NUM_NEURONS),
+    .ACTIVATION(LAYERS[0].ACTIVATION)
 ) layer_0 (
-   .clock(clock), .reset(reset),
-   .inputs_ready(inputs_ready),
-   .inputs(inputs),
-   .outputs(layer_0_outputs),
-   .outputs_ready(layer_0_outputs_ready)
+    .clock(clock), .reset(reset),
+    .inputs_ready(inputs_ready),
+    .inputs(inputs),
+    .outputs(layer_0_outputs),
+    .outputs_ready(layer_0_outputs_ready)
 );
 
 logic signed [DATA_WIDTH-1:0] layer_1_outputs[LAYERS[1].NUM_NEURONS];
 logic layer_1_outputs_ready;
 
 dense_layer #(
-   .DATA_WIDTH(DATA_WIDTH),
-   .NUM_INPUTS(LAYERS[0].NUM_NEURONS),
-   .NUM_NEURONS(LAYERS[1].NUM_NEURONS),
-   .ACTIVATION(LAYERS[1].ACTIVATION)
+    .INTG_WIDTH(INTG_WIDTH),
+    .FRAC_WIDTH(FRAC_WIDTH),
+    .NUM_INPUTS(LAYERS[0].NUM_NEURONS),
+    .NUM_NEURONS(LAYERS[1].NUM_NEURONS),
+    .ACTIVATION(LAYERS[1].ACTIVATION)
 ) layer_1 (
-   .clock(clock), .reset(reset),
-   .inputs_ready(layer_0_outputs_ready),
-   .inputs(layer_0_outputs),
-   .outputs(layer_1_outputs),
-   .outputs_ready(layer_1_outputs_ready)
+    .clock(clock), .reset(reset),
+    .inputs_ready(layer_0_outputs_ready),
+    .inputs(layer_0_outputs),
+    .outputs(layer_1_outputs),
+    .outputs_ready(layer_1_outputs_ready)
 );
 
 dense_layer #(
-   .DATA_WIDTH(DATA_WIDTH),
-   .NUM_INPUTS(LAYERS[1].NUM_NEURONS),
-   .NUM_NEURONS(LAYERS[2].NUM_NEURONS),
-   .ACTIVATION(LAYERS[2].ACTIVATION)
+    .INTG_WIDTH(INTG_WIDTH),
+    .FRAC_WIDTH(FRAC_WIDTH),
+    .NUM_INPUTS(LAYERS[1].NUM_NEURONS),
+    .NUM_NEURONS(LAYERS[2].NUM_NEURONS),
+    .ACTIVATION(LAYERS[2].ACTIVATION)
 ) output_layer (
-   .clock(clock), .reset(reset),
-   .inputs_ready(layer_1_outputs_ready),
-   .inputs(layer_1_outputs),
-   .outputs(outputs),
-   .outputs_ready(outputs_ready)
+    .clock(clock), .reset(reset),
+    .inputs_ready(layer_1_outputs_ready),
+    .inputs(layer_1_outputs),
+    .outputs(outputs),
+    .outputs_ready(outputs_ready)
 );
 
 endmodule
