6f6c7728434012f3f906f54cea68fb87169e466b
Erwan Régy <81965251+erwanregy@users.noreply.github.com>
2023-03-06 15:22:36 +0000

    Updated modules to use include header and its types

--- sources_1/new/neural_network.sv
+++ sources_1/new/neural_network.sv
@@ -1,69 +1,134 @@
-`timescale 1ns / 100ps
+`timescale 1ns / 1ps
 
-typedef struct {
-    int NUM_NEURONS;
-    ACTIVATION ACTIVATION;
-} LAYER;
+`include "include.svh"
 
 module neural_network #(parameter
-    DATA_WIDTH = 32,
-    NUM_LAYERS = 3,
-    NUM_INPUTS = 10,
-    LAYER LAYERS[NUM_LAYERS] = '{
-        '{16, RELU},
-        '{16, RELU},
-        '{10, SIGMOID}
+    int NUM_LAYERS = 4,
+    layer_builder LAYERS[NUM_LAYERS] = '{
+        '{INPUT, 10, NONE},
+        '{DENSE, 16, RELU},
+        '{DENSE, 16, RELU},
+        '{DENSE, 10, SIGMOID}
     }
 ) (
     input logic clock, reset, inputs_ready,
-    input logic signed [DATA_WIDTH-1:0] inputs[NUM_INPUTS],
-    output logic signed [DATA_WIDTH-1:0] outputs[LAYERS[NUM_LAYERS-1].NUM_NEURONS],
+    input fixed_point inputs[LAYERS[0].SIZE],
+    output fixed_point outputs[LAYERS[NUM_LAYERS-1].SIZE],
     output logic outputs_ready
 );
+    
+    fixed_point layer_1_inputs[LAYERS[0].SIZE];
+    logic layer_1_inputs_ready;
+    
+    assign layer_1_inputs = inputs;
+    assign layer_1_inputs_ready = inputs_ready;
 
-logic signed [DATA_WIDTH-1:0] layer_0_outputs[LAYERS[0].NUM_NEURONS];
-logic layer_0_outputs_ready;
+    fixed_point layer_1_outputs[LAYERS[1].SIZE];
+    logic layer_1_outputs_ready;
+    
+    dense_layer #(
+       .NUM_INPUTS(LAYERS[0].SIZE),
+       .NUM_NEURONS(LAYERS[1].SIZE),
+       .ACTIVATION(LAYERS[1].ACTIVATION)
+    ) layer_1 (
+       .clock(clock), .reset(reset),
+       .inputs_ready(layer_1_inputs_ready),
+       .inputs(layer_1_inputs),
+       .outputs(layer_1_outputs),
+       .outputs_ready(layer_1_outputs_ready)
+    );
+    
+    fixed_point layer_2_inputs[LAYERS[1].SIZE];
+    logic layer_2_inputs_ready;
+    
+    assign layer_2_inputs = layer_1_outputs;
+    assign layer_2_inputs_ready = layer_1_outputs_ready;
+    
+    fixed_point layer_2_outputs[LAYERS[2].SIZE];
+    logic layer_2_outputs_ready;
+    
+    dense_layer #(
+       .NUM_INPUTS(LAYERS[1].SIZE),
+       .NUM_NEURONS(LAYERS[2].SIZE),
+       .ACTIVATION(LAYERS[2].ACTIVATION)
+    ) layer_2 (
+       .clock(clock), .reset(reset),
+       .inputs_ready(layer_2_inputs_ready),
+       .inputs(layer_2_inputs),
+       .outputs(layer_2_outputs),
+       .outputs_ready(layer_2_outputs_ready)
+    );
+    
+    fixed_point layer_3_inputs[LAYERS[2].SIZE];
+    logic layer_3_inputs_ready;
+    
+    assign layer_3_inputs = layer_2_outputs;
+    assign layer_3_inputs_ready = layer_2_outputs_ready;
+    
+    fixed_point layer_3_outputs[LAYERS[3].SIZE];
+    logic layer_3_outputs_ready;
+    
+    dense_layer #(
+       .NUM_INPUTS(LAYERS[2].SIZE),
+       .NUM_NEURONS(LAYERS[3].SIZE),
+       .ACTIVATION(LAYERS[3].ACTIVATION)
+    ) layer_3 (
+       .clock(clock), .reset(reset),
+       .inputs_ready(layer_3_inputs_ready),
+       .inputs(layer_3_inputs),
+       .outputs(layer_3_outputs),
+       .outputs_ready(layer_3_outputs_ready)
+    );
+    
+    assign outputs = layer_3_outputs;
+    assign outputs_ready = layer_3_outputs_ready;
 
-dense_layer #(
-   .DATA_WIDTH(DATA_WIDTH),
-   .NUM_INPUTS(NUM_INPUTS),
-   .NUM_NEURONS(LAYERS[0].NUM_NEURONS),
-   .ACTIVATION(LAYERS[0].ACTIVATION)
-) layer_0 (
-   .clock(clock), .reset(reset),
-   .inputs_ready(inputs_ready),
-   .inputs(inputs),
-   .outputs(layer_0_outputs),
-   .outputs_ready(layer_0_outputs_ready)
-);
 
-logic signed [DATA_WIDTH-1:0] layer_1_outputs[LAYERS[1].NUM_NEURONS];
-logic layer_1_outputs_ready;
+    // State machine
 
-dense_layer #(
-   .DATA_WIDTH(DATA_WIDTH),
-   .NUM_INPUTS(LAYERS[0].NUM_NEURONS),
-   .NUM_NEURONS(LAYERS[1].NUM_NEURONS),
-   .ACTIVATION(LAYERS[1].ACTIVATION)
-) layer_1 (
-   .clock(clock), .reset(reset),
-   .inputs_ready(layer_0_outputs_ready),
-   .inputs(layer_0_outputs),
-   .outputs(layer_1_outputs),
-   .outputs_ready(layer_1_outputs_ready)
-);
+    localparam STATE_WIDTH = $clog2(1 + NUM_LAYERS + 1);
 
-dense_layer #(
-   .DATA_WIDTH(DATA_WIDTH),
-   .NUM_INPUTS(LAYERS[1].NUM_NEURONS),
-   .NUM_NEURONS(LAYERS[2].NUM_NEURONS),
-   .ACTIVATION(LAYERS[2].ACTIVATION)
-) output_layer (
-   .clock(clock), .reset(reset),
-   .inputs_ready(layer_1_outputs_ready),
-   .inputs(layer_1_outputs),
-   .outputs(outputs),
-   .outputs_ready(outputs_ready)
-);
+    enum logic [STATE_WIDTH-1:0] {idle, layer_1_processing, layer_2_processing, layer_3_processing, done} present_state, next_state;
+    
+    always_ff @(posedge clock, posedge reset) begin
+        if (reset) begin
+            present_state <= idle;
+        end else begin
+            present_state <= next_state;
+        end
+    end
+
+    always_comb begin
+        next_state = present_state;
+        unique case (present_state)
+            idle: begin
+                if (inputs_ready) begin
+                    next_state = layer_1_processing;
+                end else begin
+                    next_state = idle;
+                end
+            end layer_1_processing: begin
+                if (layer_1_outputs_ready) begin
+                    next_state = layer_2_processing;
+                end else begin
+                    next_state = layer_1_processing;
+                end
+            end layer_2_processing: begin
+                if (layer_2_outputs_ready) begin
+                    next_state = layer_3_processing;
+                end else begin
+                    next_state = layer_2_processing;
+                end
+            end layer_3_processing: begin
+                if (layer_3_outputs_ready) begin
+                    next_state = idle;
+                end else begin
+                    next_state = layer_3_processing;
+                end
+            end default: begin
+                next_state = idle;
+            end
+        endcase
+    end
 
 endmodule
--- sources_1/new/neuron.sv
+++ sources_1/new/neuron.sv
@@ -1,156 +1,225 @@
-`timescale 1ns / 100ps
+`timescale 1ns / 1ps
+
+`include "include.svh"
 
 module neuron #(parameter
-    DATA_WIDTH = 32,
-    NUM_INPUTS = 16,
-    ACTIVATION = RELU
+    int NUM_INPUTS = 16,
+    activation_type ACTIVATION = RELU
 ) (
-    input logic clock, reset, input_ready,
-    input logic signed [DATA_WIDTH-1:0] inputs[NUM_INPUTS],
-    output logic signed [DATA_WIDTH-1:0] out,
+    input logic clock, reset, inputs_ready,
+    input fixed_point inputs[NUM_INPUTS],
+    output fixed_point out,
     output logic output_ready
 );
 
-
-// Weights
-
-logic signed [DATA_WIDTH-1:0] weights[NUM_INPUTS];
-
-initial begin
-    foreach (weights[i]) begin
-        weights[i] = i;
-    end
-end
-
-
-// Bias
-
-logic signed [DATA_WIDTH-1:0] bias;
-
-initial begin
-    bias = 0;
-end
-
-
-// State machine outputs
-
-logic count, reset_accumulator, reset_multipliers;
-
-
-// Multiply
-
-logic signed [(2*DATA_WIDTH)-1:0] products[NUM_INPUTS];
-
-always_ff @(posedge clock or posedge reset) begin : multiply
-    if (reset) begin
-        foreach (products[i]) begin
-            products[i] <= 0;
-        end
-    end else if (reset_multipliers) begin
-        foreach (products[i]) begin
-            products[i] <= 0;
-        end
-    end else begin
-        foreach (products[i]) begin
-            products[i] <= inputs[i] * weights[i];
+    localparam DATA_WIDTH = INTEGRAL_WIDTH + FRACTION_WIDTH;
+    
+    
+    // Weights
+    
+    localparam NUM_WEIGHTS = NUM_INPUTS;
+    
+    fixed_point weights[NUM_WEIGHTS];
+    
+    initial begin
+        foreach (weights[i]) begin
+            weights[i].integral = 0;
+            weights[i].integral[INTEGRAL_WIDTH-1] = $random;
+            weights[i].fraction = $urandom_range((2 ** (FRACTION_WIDTH - 1)) / 10);
         end
     end
-end
-
-
-// Input number counter
-
-logic [$clog2(NUM_INPUTS)-1:0] input_num;
-
-always_ff @(posedge clock or posedge reset) begin
-    if (reset) begin
-        input_num <= 0;
-    end else if (reset_accumulator) begin
-        input_num <= 0;
-    end else if (count) begin
-        input_num <= input_num + 1;
-    end else begin
-        input_num <= input_num;
+    
+    
+    // Bias
+    
+    fixed_point bias;
+    
+    initial begin
+        bias = 0;
     end
-end
-
-
-// Accumulate
-
-logic signed [($clog2(NUM_INPUTS)+2*DATA_WIDTH)-1:0] sum;
-
-always_ff @(posedge clock or posedge reset) begin : accumulate
-    if (reset || reset_accumulator) begin
-        sum <= 0;
-    end else begin
-        sum <= sum + products[input_num];
-    end
-end
-
-
-// Activate
-
-always_ff @(posedge clock or posedge reset) begin : activate
-    if (reset) begin
-        out <= 0;
-    end else begin
-        out <= (sum > 0) ? sum : 0;
-    end
-end
-
-
-// State machine
-
-enum logic [2:0] {waiting, multiplying, accumulating, activating, done} present_state, next_state;
-
-always_ff @(posedge clock, posedge reset) begin
-    if (reset) begin
-        present_state <= waiting;
-    end else begin
-        present_state <= next_state;
-    end
-end
-
-always_comb begin
-    count = 0;
-    reset_accumulator = 0;
-    reset_multipliers = 0;
-    output_ready = 0;
-    next_state = present_state;
-    unique case (present_state)
-        waiting: begin
-            reset_accumulator = 1;
-            reset_multipliers = 1;
-            if (input_ready) begin
-                next_state = multiplying;
-            end else begin
-                next_state = waiting;
-            end 
+    
+    
+    // State machine outputs
+    
+    logic reset_units, multiply, accumulate, activate;
+    
+    
+    // Multiplication
+    
+    localparam PRODUCT_WIDTH = 2 * DATA_WIDTH;
+    localparam NUM_PRODUCTS = NUM_INPUTS;
+    
+    logic signed [PRODUCT_WIDTH-1:0] products[NUM_PRODUCTS];
+    
+    always_ff @(posedge clock or posedge reset) begin: multipliers
+        if (reset) begin
+            foreach (products[i]) begin
+                products[i] <= 0;
+            end
+        end else if (reset_units) begin
+            foreach (products[i]) begin
+                products[i] <= 0;
+            end
+        end else if (multiply) begin
+            foreach (products[i]) begin
+                products[i] <= inputs[i] * weights[i];
+            end
+        end else begin
+            foreach (products[i]) begin
+                products[i] <= products[i];
+            end
         end
-        multiplying: begin
-            next_state = accumulating;
+    end
+    
+    
+    // Accumulation
+    
+    localparam PRODUCT_NUM_WIDTH = $clog2(NUM_INPUTS);
+    
+    logic [PRODUCT_NUM_WIDTH-1:0] product_num;
+    
+    always_ff @(posedge clock or posedge reset) begin: product_num_counter
+        if (reset) begin
+            product_num <= 0;
+        end else if (reset_units) begin
+            product_num <= 0;
+        end else if (accumulate) begin
+            product_num <= product_num + 1;
+        end else begin
+            product_num <= product_num;
         end
-        accumulating: begin
-            count = 1;
-            if (input_num == NUM_INPUTS - 1) begin
-                next_state = activating;
-            end else if (input_num < NUM_INPUTS) begin
+    end
+    
+    localparam SUM_WIDTH = PRODUCT_WIDTH + PRODUCT_NUM_WIDTH;
+    
+    typedef struct packed {
+        logic signed [SUM_WIDTH-FRACTION_WIDTH-1:0] integral;
+        logic [FRACTION_WIDTH-1:0] fraction;
+    } fixed_point_sum;
+    
+    fixed_point_sum sum;
+    
+    always_ff @(posedge clock or posedge reset) begin : accumulator
+        if (reset) begin
+            sum <= 0;
+        end else if (reset_units) begin
+            sum <= 0;
+        end else if (accumulate) begin
+            // TODO: parallelise or optimise
+            sum <= sum + products[product_num];
+        end else begin
+            sum <= sum;
+        end
+    end
+    
+    
+    // Activation
+    // TODO: add sigmoid
+    
+    // I want to have 2**FRACTION_WIDTH - 1 number of entries in sigmoid to keep FRAC_WIDTH precision
+    // values of sum greater than or less than the range specified can be rounded to 1 or 0 respectively
+    // need to calculate a reasonable domain to calc sigmoid values for, (depending on frac_width precision?)
+    // then precompute these values and store in sigmoid
+
+    localparam NUM_SIGMOID_ENTRIES = 2 ** FRACTION_WIDTH - 1;
+    
+    logic signed [DATA_WIDTH-1:0] sigmoid[-NUM_SIGMOID_ENTRIES/2:NUM_SIGMOID_ENTRIES/2 - 1]; // TODO: only generate sigmoid rom if ACTIVATION is SIGMOID
+    
+    initial begin
+        foreach (sigmoid[i]) begin
+
+        end
+    end
+    
+    always_ff @(posedge clock or posedge reset) begin : activator
+        if (reset) begin
+            out <= 0;
+        end else if (reset_units) begin
+            out <= 0;
+        end else if (activate) begin
+            unique case (ACTIVATION)
+                RELU: begin
+                    if (sum[SUM_WIDTH-1] == 0) begin
+                        if (sum >= (2 ** DATA_WIDTH - 1)) begin
+                            out <= 2 ** (DATA_WIDTH - 1) - 1;
+                        end else begin
+                            out <= sum;
+                        end
+                    end else begin
+                        out <= 0;
+                    end
+                end SIGMOID: begin
+                    if (sum >= NUM_SIGMOID_ENTRIES/2) begin
+                        out <= 1 << FRACTION_WIDTH;
+                    end else if (sum < -NUM_SIGMOID_ENTRIES/2) begin
+                        out <= 0;
+                    end else begin
+                        out <= sigmoid[sum.fraction];
+                    end
+                end default: begin
+                    $fatal("Invalid activation function %s", ACTIVATION.name());
+                end
+            endcase
+        end else begin
+            out <= out;
+        end
+    end
+    
+    
+    // State machine
+    
+    enum logic [2:0] {waiting, multiplying, accumulating, activating, done} present_state, next_state;
+    
+    always_ff @(posedge clock, posedge reset) begin
+        if (reset) begin
+            present_state <= waiting;
+        end else begin
+            present_state <= next_state;
+        end
+    end
+    
+    always_comb begin
+        reset_units = 0;
+        multiply = 0;
+        accumulate = 0;
+        activate = 0;
+        output_ready = 0;
+        next_state = present_state;
+        unique case (present_state)
+            waiting: begin
+                reset_units = 1;
+                if (inputs_ready) begin
+                    next_state = multiplying;
+                end else begin
+                    next_state = waiting;
+                end 
+            end
+            multiplying: begin
+                multiply = 1;
                 next_state = accumulating;
-            end else begin
+            end
+            accumulating: begin
+                accumulate = 1;
+                if (product_num < NUM_PRODUCTS - 1) begin
+                    next_state = accumulating;
+                end else if (product_num == NUM_PRODUCTS - 1) begin
+                    next_state = activating;
+                end else begin
+                    next_state = waiting;
+                end 
+            end
+            activating: begin
+                activate = 1;
+                next_state = done;
+            end
+            done: begin
+                output_ready = 1;
                 next_state = waiting;
-            end 
-        end
-        activating: begin
-            next_state = done;
-        end
-        done: begin
-            output_ready = 1;
-            next_state = done;
-        end
-        default: begin
-            next_state = waiting;
-        end
-    endcase
-end
+            end
+            default: begin
+                next_state = waiting;
+            end
+        endcase
+    end
 
 endmodule
