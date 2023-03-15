82cc5e6f15a744e1266ddfbb77ec4f0a2df1ad60
Erwan Régy <erwanregy@users.noreply.github.com>
2023-03-15 16:23:37 +0000

    Updated float_to_fixed to have width arguments in all funcs

--- float_to_fixed.py
+++ float_to_fixed.py
@@ -1,38 +1,35 @@
 import os
 
-integer_width, fraction_width = 8, 8
 
-
-def float_to_fixed(line: str) -> str:
-    integer = int(round(float(line) * 2**fraction_width))
+def float_to_fixed(value: float, integer_width: int, fraction_width: int) -> str:
+    integer = int(round(value * 2**fraction_width))
     if integer < 0:
         integer = (1 << (integer_width + fraction_width)) + integer
     return bin(integer)[2:].zfill(integer_width + fraction_width)
 
 
-def float_to_fixed_file(file_path: str) -> None:
+def float_to_fixed_file(file_path: str, integer_width: int, fraction_width: int) -> None:
     with open(file_path, "r") as file:
         lines = file.readlines()
     with open(file_path, "w") as file:
         for line in lines:
-            file.write(float_to_fixed(line) + "\n")
+            file.write(float_to_fixed(float(line), integer_width, fraction_width) + "\n")
 
 
-def float_to_fixed_model(model_path: str) -> None:
+def float_to_fixed_model(model_path: str, integer_width: int, fraction_width: int) -> None:
     for layer in os.listdir(model_path):
         layer_path = os.path.join(model_path, layer)
         for neuron in os.listdir(layer_path):
             neuron_path = os.path.join(layer_path, neuron)
             for parameter in os.listdir(neuron_path):
                 parameter_path = os.path.join(neuron_path, parameter)
-                float_to_fixed_file(parameter_path)
+                float_to_fixed_file(parameter_path, integer_width, fraction_width)
 
 
 def main() -> None:
-    float_to_fixed_model("models/3b1b")
+    integer_width, fraction_width = 8, 8
+    float_to_fixed_model("models/3b1b", integer_width, fraction_width)
 
 
 if __name__ == "__main__":
-    # main()
-    float_to_fixed_file("weights.txt")
-    float_to_fixed_file("bias.txt")
+    main()
